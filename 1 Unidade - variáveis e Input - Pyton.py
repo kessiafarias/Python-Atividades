1f3133705c26da54454e9b0b495daab1fbf41bcc
kessiafarias <kessiaoliveiraadm@hotmail.com>
2022-09-24 22:47:00 -0300

    Adicionado cidade

--- 1 Unidade - variáveis e Input - Pyton.py
+++ 1 Unidade - variáveis e Input - Pyton.py
@@ -4,10 +4,12 @@ print("Olá Mundo!!")
 nome = input ("Nome e Sobrenome: ")
 idade = int(input("Qual sua idade: "))
 email = input("Qual seu e-mail: ")
+cidade = input("Qual sua cidade: ")
 
 print ("Seu nome e Sobrenome é: " , nome)
 print ("Sua idade é: " , idade)           
 print ("Seu e-mail é: ", email)
+print ("Qual sua cidade: ", cidade)
 
 
 
